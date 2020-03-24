17fd61038983a132f46a725c50de0eb472bda34e
Shaohon_Chen <611699999@qq.com>
2020-03-24 18:30:09 +0800

    new

---
+++ test.py
@@ -0,0 +1,21 @@
+import os.path
+
+
+
+def laod_test_data_list():
+    
+
+
+def main():
+    # load test data
+    laod_test_data_list()
+
+
+if __name__=="__main__":
+    # success run
+    print("successful open test.py")
+    
+    main()
+
+    # end run
+    print("successsful end test.py")
