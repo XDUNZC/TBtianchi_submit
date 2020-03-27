502f95260016dc24f2ffe3d48a8e5c3a942e0eac
Shaohon_Chen <611699999@qq.com>
2020-03-27 22:06:22 +0800

    change run

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from read_dataset import Reader
+from save import Saver
 
 
 def main():
     # 初始化文件路径获得类
     test_path_reader = Reader()
     # 初始化结果保存类
-
+    saver = Saver()
     # 执行匹配工作
 
     # 执行画框工作
