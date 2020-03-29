cd780cdc66eb32ee8f0a1d936a6ff8a90f2fa1d8
Shaohon Chen <shaohon_chen@foxmail.com>
2020-03-29 10:08:48 +0000

    fix run.py load image

--- model/Resnet50/run.py
+++ model/Resnet50/run.py
@@ -7,8 +7,8 @@ from .model import Siamese_ResNet, Bottleneck
 
 class Worker:
     def __init__(self,
-                 model_path="./model/Resnet50/models/",# TODO 改变model路径
-                 gpu_list="0"):
+            model_path="./model/Resnet50/models/"): # TODO 改变model路径
+                 
         """
         初始化模型
         :param model_path:
@@ -16,14 +16,15 @@ class Worker:
         """
         self.Flags = gflags.FLAGS
         gflags.DEFINE_bool("cuda", True, "use cuda")
-        gflags.DEFINE_string("gpu_ids", gpu_list, "gpu ids used to train")
+        gflags.DEFINE_string("gpu_ids", "0", "gpu ids used to train")
 
         # 创建模型
         self.net = Siamese_ResNet([3, 4, 6, 3])
         # 加载模型参数
         model = torch.load(model_path)
-        model_dict = model.state_dict()
-        self.net.load_state_dict(model_dict)
+        # model_dict = model.state_dict()
+        # self.net.load_state_dict(model_dict)
+        self.net.load_state_dict(model)
 
         # 加载整个模型
         # self.net = torch.load(model_path)
@@ -34,18 +35,24 @@ class Worker:
 
         if self.Flags.cuda:
             self.net.cuda()
-        self.net.test()
+        self.net.eval()
 
-    def get_match_value(self, img_a: torch, img_b: torch):
+    def get_match_value(self, img_a: numpy, img_b: numpy):
         """
         获得匹配值
-        :param img_a:第一个图像 torch
-        :param img_b:第二个图像 torch
+        :param img_a:第一个图像 numpy(H*W*C)
+        :param img_b:第二个图像 numpy(H*W*C
         :return:
         """
+        img_a=torch.from_numpy(img_a)
+        img_b=torch.from_numpy(img_b)
+        if len(img_a.shape)==3: # 转化为batch
+            img_a=img_a.unsqueeze(0)
+        if len(img_b.shape)==3: # 转化为batch
+            img_b=img_b.unsqueeze(0)
         with torch.no_grad():
             if self.Flags.cuda:
-                img_a, img_b = img_a.cuda(), img_b.test2.cuda()
+                img_a, img_b = img_a.cuda(), img_b.cuda()
             test1, test2 = Variable(img_a), Variable(img_b)
             output = self.net.forward(test1, test2).data.cpu().numpy()
         return output
